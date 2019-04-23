d75ff34df902e2e99a4b2b4d600da4f3822191da
Dawid Fulmes <fulmes.dawid@gmail.com>
2019-04-23 13:57:22 +0200

    Add EditDeck component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import EditDeck from './EditDeck/EditDeck';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello world
+        <EditDeck />
       </div>
     );
   }
---
+++ src/components/EditDeck/EditDeck.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EditDeck = () => {
+    return (
+        <div>
+            <p>Adding Form</p>
+            <p>CardsList</p>
+        </div>
+    );
+}
+
+export default EditDeck;
