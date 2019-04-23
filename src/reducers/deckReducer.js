112760a86366454720f29e16eb1420ad60d9e22b
Dawid Fulmes <fulmes.dawid@gmail.com>
2019-04-23 12:24:15 +0200

    Add default deck to deckReducer

--- src/reducers/deckReducer.js
+++ src/reducers/deckReducer.js
@@ -1,4 +1,9 @@
-const deckReducer = (state = [], action) => {
+const defaultDeck = [
+    { eng: "yes", pol: "tak" },
+    { eng: "no", pol: "nie" }
+]
+
+const deckReducer = (state = defaultDeck, action) => {
     switch (action.type) {
         default:
             return state
