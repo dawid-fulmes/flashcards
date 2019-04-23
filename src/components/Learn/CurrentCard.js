fa72aafcad8473047eb139908ad238e125c35db1
Dawid Fulmes <fulmes.dawid@gmail.com>
2019-04-23 19:49:36 +0200

    Create CurrentCard component and add it to Learn

---
+++ src/components/Learn/CurrentCard.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class CurrentCard extends Component {
+    state = {
+        isTurned: false
+    }
+    render() {
+        return (
+            <div>
+                CurrentCard
+            </div>
+        );
+    }
+}
+
+export default CurrentCard;
