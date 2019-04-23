905233ce3713d381510b84882738dec7e6066068
Dawid Fulmes <fulmes.dawid@gmail.com>
2019-04-23 17:32:14 +0200

    Add AddCardForm structure and controlled inputs

--- src/components/EditDeck/AddCardForm.js
+++ src/components/EditDeck/AddCardForm.js
@@ -2,14 +2,27 @@ import React, { Component } from 'react';
 
 class AddCardForm extends Component {
     state = {
-        polInput: '',
-        engInput: ''
+        engInput: '',
+        polInput: ''
+    }
+    handleChange = (e) => {
+        const { name, value } = e.target;
+        this.setState({ [name]: value });
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
     }
     render() {
+        const { polInput, engInput } = this.state
+        const { handleChange, handleSubmit } = this
         return (
             <div>
-                <form>
-                    Form
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="eng">eng:</label>
+                    <input type="text" id="eng" value={engInput} name="engInput" onChange={handleChange} />
+                    <label htmlFor="pol">pol:</label>
+                    <input type="text" id="pol" value={polInput} name="polInput" onChange={handleChange} />
+                    <button>Add</button>
                 </form>
             </div>
         );
