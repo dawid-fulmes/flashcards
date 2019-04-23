c8aee5693b0330825e245296957173fb12159933
Dawid Fulmes <fulmes.dawid@gmail.com>
2019-04-23 18:09:26 +0200

    Add simple form validation in AddCardForm

--- src/components/EditDeck/AddCardForm.js
+++ src/components/EditDeck/AddCardForm.js
@@ -5,24 +5,44 @@ import { addCard } from '../../actions/actions'
 class AddCardForm extends Component {
     state = {
         engInput: '',
-        polInput: ''
+        polInput: '',
+        errorVisible: false
     }
+
     handleChange = (e) => {
         const { name, value } = e.target;
         this.setState({ [name]: value });
     }
+
     handleSubmit = (e) => {
         e.preventDefault();
         const { polInput, engInput } = this.state;
-        this.props.dispatch(addCard(engInput, polInput));
-        this.setState({
-            engInput: '',
-            polInput: ''
-        });
+        if (polInput.trim().length > 0 && engInput.trim().length > 0) {
+            this.props.dispatch(addCard(engInput, polInput));
+            this.setState({
+                engInput: '',
+                polInput: '',
+                errorVisible: false
+            });
+        } else {
+            this.setState({
+                errorVisible: true
+            });
+        }
     }
+
+    componentDidUpdate() {
+        if (this.state.errorVisible) {
+            setTimeout(() => this.setState({
+                errorVisible: false
+            }), 3000)
+        }
+    }
+
     render() {
-        const { polInput, engInput } = this.state;
+        const { polInput, engInput, errorVisible } = this.state;
         const { handleChange, handleSubmit } = this;
+        const errorMessage = <p className="errorMessage">Each input need at least one letter!</p>;
         return (
             <div>
                 <form onSubmit={handleSubmit}>
@@ -31,6 +51,7 @@ class AddCardForm extends Component {
                     <label htmlFor="pol">pol:</label>
                     <input type="text" id="pol" value={polInput} name="polInput" onChange={handleChange} />
                     <button>Add</button>
+                    {errorVisible ? errorMessage : null}
                 </form>
             </div>
         );
