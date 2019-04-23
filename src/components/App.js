9970f0b2bf3211cf33cdacd07e5a93ba79d1dd96
Dawid Fulmes <fulmes.dawid@gmail.com>
2019-04-23 19:44:06 +0200

    Create Learn component and add it to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import EditDeck from './EditDeck/EditDeck';
+import Learn from './Learn/Learn';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <EditDeck />
+        <Learn />
       </div>
     );
   }
---
+++ src/components/Learn/Learn.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Learn = () => {
+    return (
+        <div>CurrentCard</div>
+    );
+}
+
+export default Learn;
