4311fbdd24034093ada4666e0bed8b22f14f3b08
Dawid Fulmes <fulmes.dawid@gmail.com>
2019-04-24 12:19:47 +0200

    Connect App to Redux and display modes according to store

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import EditDeck from './EditDeck/EditDeck';
 import Learn from './Learn/Learn';
 import Header from './Header'
 
 class App extends Component {
   render() {
+    const { appMode } = this.props
     return (
       <div className="App">
         <Header />
-        <EditDeck />
-        <Learn />
+        {appMode === "EDIT_MODE" ? <EditDeck /> : null}
+        {appMode === "LEARN_MODE" ? <Learn /> : null}
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  appMode: state.appMode
+})
+
+export default connect(
+  mapStateToProps,
+  null
+)(App);
