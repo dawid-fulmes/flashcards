1b4d71a696ae5fe63531cc7a2a770bc6d4e5cd73
Dawid Fulmes <fulmes.dawid@gmail.com>
2019-04-24 11:49:01 +0200

    Add mechanism of showing answer on click in CurrentCard

--- src/components/Learn/CurrentCard.js
+++ src/components/Learn/CurrentCard.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
 class CurrentCard extends Component {
     state = {
         isTurned: false
     }
+    handleClick = () => {
+        this.setState({ isTurned: true });
+    }
     render() {
+        const { isTurned } = this.state
+        const { currentCard } = this.props
+        const { handleClick } = this
         return (
-            <div>
-                CurrentCard
+            <div className="current-card">
+                <div onClick={handleClick}>
+                    {currentCard.pol}
+                    {isTurned ? currentCard.eng : null}
+                </div>
             </div>
         );
     }
 }
 
-export default CurrentCard;
+const mapStateToProps = (state) => ({
+    currentCard: state.deck[0]
+})
+
+export default connect(
+    mapStateToProps,
+    null
+)(CurrentCard);
