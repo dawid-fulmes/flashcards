71d2925920d930356df0d217524f1f2cdeecd55e
Dawid Fulmes <fulmes.dawid@gmail.com>
2019-04-24 11:59:10 +0200

    Add Header with title and menu to App

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Flashcard</h1>
+            <nav>
+                <ul>
+                    <li>Learn</li>
+                    <li>Edit Deck</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
